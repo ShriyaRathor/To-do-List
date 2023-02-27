3b3b0b32e245fe97ff2ef6a451a003c01ec61111
Shriya <rathor.k.shriya@gmail.com>
2023-02-27 16:49:12 +0530

    add initial function of add note

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ var date = new Date();
 var day = weekday[date.getDay()];
 var current_date = date.getDate() + "-" + (date.getMonth()+1)+"-" + date.getFullYear()+", " + day ;
 document.getElementById("currentDay").innerHTML = current_date;
+
+
+var newinput = document.getElementById("new-input")
+newinput.addEventListener("keypress", function(event)
+{
+    if(event.key == "Enter"){
+        var div = document.getElementById('section')
+        var p = document.createElement("p");
+        p.innerHTML = newinput.value;
+        div.append(p)
+        newinput.value = "";
+    }
+});
+
+
